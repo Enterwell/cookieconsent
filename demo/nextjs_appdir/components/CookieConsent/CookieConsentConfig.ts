993cb88a006be90fadb394cda0fd5f12b8159e05
DorijanH <dhaspl@gmail.com>
2024-06-11 13:06:01 +0200

    feat: Nextjs demo app fetching vendor-list

--- demo/nextjs_appdir/components/CookieConsent/CookieConsentConfig.ts
+++ demo/nextjs_appdir/components/CookieConsent/CookieConsentConfig.ts
@@ -15,10 +15,10 @@ const config: CookieConsentConfig = {
     disclosedVendorIds: [755, 12, 32, 416],
     cmpId: 1_500,
     cmpVersion: 1,
-    useMockGvl: true,
-    gvlBaseUrl: 'https://jsonplaceholder.typicode.com/todos',
-    gvlDefaultFileName: '1',
-    gvlLanguageFileName: '1'
+    // useMockGvl: false,
+    gvlBaseUrl: 'http://localhost:3000'
+    // gvlDefaultFileName: 'vendor-list.json',
+    // gvlLanguageFileName: 'vendor-list-[LANG].json'
   },
 
   cookie: {
@@ -183,10 +183,96 @@ const config: CookieConsentConfig = {
       },
       hr: {
         consentModal: {
-          description: 'We and our partners ({{count}}) store and/or access information on a device, such as unique IDs in cookies to process personal data. You may find out more about the purposes for which we and our partners use cookies or exercise your preferences by clicking the ‘Cookie settings’ button below. You can revisit your consent choices or withdraw consent at any time by clicking the link to your cookie settings in our Cookie Policy. These choices will be signaled to our partners and will not affect browsing data.'
+          title: 'Mi Poštujemo Vašu Privatnost',
+          description: 'Mi i naši partneri ({{count}}) pohranjujemo i/ili pristupamo informacijama na uređaju, kao što su jedinstveni ID-ovi u kolačićima za obradu osobnih podataka. Možete saznati više o svrhama u koje mi i naši partneri koristimo kolačiće ili iskoristiti svoje postavke klikom na gumb "Postavke kolačića" u nastavku. Možete ponovno pregledati svoje odabire pristanka ili povući pristanak u bilo kojem trenutku klikom na poveznicu na svoje postavke kolačića u našim Pravilima o kolačićima. Ovi izbori bit će signalizirani našim partnerima i neće utjecati na podatke o pregledavanju.',
+          acceptAllBtn: 'Prihvaćam sve',
+          acceptNecessaryBtn: 'Prihvaćam samo nužne',
+          showPreferencesBtn: 'Postavke kolačića',
+          showVendorsBtn: 'Popis partnera (dobavljača)',
+          vendorTitle: 'Mi i naši partneri obavljamo sljedeće na temelju vaših postavki',
+          footer: `
+                      <a href="#path-to-impressum.html" target="_blank">Impressum</a>
+                      <a href="#path-to-privacy-policy.html" target="_blank">Pravila privatnosti</a>
+                  `
         },
         preferencesModal: {
-          sections: []
+          title: 'Upravljaj postavkama kolačića',
+          acceptAllBtn: 'Prihvaćam sve',
+          acceptNecessaryBtn: 'Prihvaćam samo nužne',
+          savePreferencesBtn: 'Spremi postavke',
+          closeIconLabel: 'Zatvori modal',
+          serviceCounterLabel: 'Servis|Servisa',
+          illustrationsTitle: 'Primjeri',
+          purposeVendorCountLabel: '{{count}} partnera može koristiti ovu svrhu',
+          viewIllustrationsLabel: 'Pogledajte primjere',
+          viewVendorsLabel: 'Popis IAB dobavljača',
+          sections: [
+            {
+              title: 'O Vašoj Privatnosti',
+              description: 'Vaše podatke obrađujemo u svrhe kao što je isporuka sadržaja ili reklama i mjerenje isporuke takvog sadržaja ili reklama kako bismo dobili uvide o našoj web stranici. Ove informacije možemo podijeliti s našim partnerima. Kolačići koje smo mi postavili prikazuju se prvi i ne sadrže broj partnera pod nazivom kategorije; možete iskoristiti svoje postavke u vezi s kolačićima prve strane prebacivanjem prekidača za svaku kategoriju kolačića prve strane u nastavku. Preostali kolačići su kolačići treće strane; možete iskoristiti svoje preferencije u odnosu na svaku svrhu prebacivanjem odgovarajućeg prekidača u nastavku ili prema dobavljaču klikom na "Popis IAB dobavljača". Ovi izbori bit će signalizirani globalno našim dobavljačima koji sudjeluju u Okviru za transparentnost i pristanak (TCF).'
+            },
+            {
+              title: 'Nužni',
+              description: 'Ovi su kolačići neophodni za pravilno funkcioniranje web stranice i ne mogu se onemogućiti.',
+              //this field will generate a toggle linked to the 'necessary' category
+              linkedCategory: 'necessary'
+            },
+            {
+              title: 'Izvedba i analitika',
+              description: 'Ovi kolačići prikupljaju informacije o tome kako koristite našu web stranicu. Svi podaci su anonimizirani i ne mogu se koristiti za vašu identifikaciju.',
+              linkedCategory: 'analytics',
+              cookieTable: {
+                caption: 'Tablica kolačića',
+                headers: {
+                  name: 'Kolačić',
+                  domain: 'Domena',
+                  desc: 'Opis'
+                },
+                body: [
+                  {
+                    name: '_ga',
+                    domain: location.hostname,
+                    desc: 'Opis 1'
+                  },
+                  {
+                    name: '_gid',
+                    domain: location.hostname,
+                    desc: 'Opis 2'
+                  }
+                ]
+              }
+            },
+            {
+              title: 'Ciljanje i oglašavanje',
+              description: 'Ovi se kolačići koriste kako bi oglasne poruke bile relevantnije za vas i vaše interese. Namjera je prikazati oglase koji su relevantni i privlačni za pojedinačnog korisnika, a time i vrijedniji za izdavače i oglašivače treće strane.',
+              linkedCategory: 'ads'
+            },
+            {
+              title: 'Više informacija',
+              description: 'Za sve upite u vezi s našim pravilima o kolačićima i vašim izborima, <a href="#contact-page">kontaktirajte nas</a>'
+            }
+          ]
+        },
+        vendorsModal: {
+          title: 'IAB popis dobavljača',
+          allowAllConsentBtn: 'Dopusti sve',
+          denyAllConsentBtn: 'Zabrani sve',
+          allowSelectionBtn: 'Spremi postavke',
+          viewPrivacyPolicyLabel: 'Pogledajte Pravila privatnosti',
+          viewLegitimateInterestClaimLabel: 'Pogledajte tvrdnje za legitimni interes',
+          viewDeviceStorageDisclosureLabel: 'Pogledajte Izjavu o pohrani uređaja',
+          cookieLifespanLabel: 'Životni vijek kolačića',
+          cookieLifespanMonthsLabel: 'Mjeseca',
+          usesNonCookieAccessLabel: 'Ovaj dobavljač koristi druge metode pohrane ili pristupa informacijama osim kolačića',
+          dataDeclarationLabel: 'Izjava o podacima',
+          dataRetentionLabel: 'Zadržavanje podataka',
+          standardRetentionLabel: 'Standardno zadržavanje',
+          dataRetentionDaysLabel: 'Dana',
+          consentPurposesLabel: 'Svrhe pristanka',
+          legitimateInterestPurposesLabel: 'Svrhe legitimnog interesa',
+          specialPurposesLabel: 'Posebne namjene',
+          featuresLabel: 'Značajke',
+          specialFeaturesLabel: 'Posebne značajke'
         }
       }
     }
